e1762ed42f0cfe6e7dc66d109c7c62b0ba41d148
Scheibelr <scheibelr@gmail.com>
2022-02-13 17:24:46 -0500

    curves

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 
+
 class Home extends Component {
     constructor(props) {
         super(props);
@@ -18,28 +19,50 @@ class Home extends Component {
                     <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
                     </p>
                 </section>
+                <section class="blue">
+                    <h2>Nice Curves</h2>
+                    <p className="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
+                    This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
+                    <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
+                    <div class="wave">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                        </svg>
+                    </div>
+                    </p>
+                </section>
                 <section>
+                    <h2>Nice Curves</h2>
+                    <p className="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
+                    This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
+                    <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
+                    <div class="wave2">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
+                    </p>
+                </section>
+                <section className="red">
+                    <h2>Nice Curves</h2>
+                    <p classname="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
+                    This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
+                    <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
+                    <div class="wave">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                        </svg>
+                    </div>
+                    </p>
+                </section>
+                <section className="purple"on>
                     <h2>Nice Curves</h2>
                     <p classname="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
                     This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
                     <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
                     </p>
                 </section>
-                <section>
-                    <h2>Nice Curves</h2>
-                    <p classname="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
-                    This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
-                    <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
-                    </p>
-                </section>
-                <section>
-                    <h2>Nice Curves</h2>
-                    <p classname="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
-                    This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
-                    <br></br>Simultaneously, and at an exponentially increasing time interval, the original gen 1 creature will metamorphose to a later stage in its life. 
-                    </p>
-                </section>
-                <section>
+                <section className="indigo"on>
                     <h2>Nice Curves</h2>
                     <p classname="about-text">Speciation is an NFT game loosely based on the concept of evolution. It starts with a single minted NFT: The Dream Pool. From there, at a set interval of time, 1 week, an NFT will spawn in the same wallet. 
                     This NFT will be a generation 1 creature, with very simple attributes. It may resemble a simple worm.  Every month, this creature will have an offspring, which is a newly minted NFT creature, simlar to it, but with additional mutated traits such as eyes, limbs, teeth, etc. It will have unique traits that are randomly generated from its cohort pool. That NFT, will, in turn, mint a new NFT each month with traits from a new cohort pool that will be added on top of those traits, such as sensory organs, limbs, armor, etc.
