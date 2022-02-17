eaefb35b554a00839afc993c9c7bf6b5f5a7dea4
Scheibelr <scheibelr@gmail.com>
2022-02-16 21:37:51 -0500

    bug fix

--- src/components/ParallaxTextScrollComponent.js
+++ src/components/ParallaxTextScrollComponent.js
@@ -14,7 +14,7 @@ class ParallaxTextScroll extends Component {
 
     const position = document.documentElement;
     position.addEventListener("mousemove", e => {
-        position.style.setProperty('--x', e.clientX + 'px')
+        position.style.setProperty('--x', e.clientY + 'px')
     });
 
     const scrollText = {
